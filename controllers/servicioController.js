957860a4cbc9c3a0b3ad470cb72150f8fa6e5afc
lRen1505 <renso.escuadra@usil.pe>
2026-03-27 09:49:07 -0500

    Se cambio el servicio que se esta brindando

--- controllers/servicioController.js
+++ controllers/servicioController.js
@@ -1,6 +1,22 @@
 const servicios = [
-  { id: 1, nombre: "Carry Mítico+", precio: 50 },
-  { id: 2, nombre: "Carry Raid Heroico", precio: 100 }
+  {
+    id: 1,
+    nombre: "Desarrollo de página web",
+    descripcion: "Creación de landing page o sitio web corporativo",
+    precio: 300 
+  },
+  {
+    id: 2,
+    nombre: "Diseño de logo",
+    descripcion: "Diseño de identidad visual para marca o negocio",
+    precio: 80
+  },
+  {
+    id: 3,
+    nombre: "Dashboard en Power BI",
+    descripcion: "Desarrollo de reportes y paneles interactivos",
+    precio: 150
+  }
 ];
 
 const obtenerServicios = (req, res) => {
