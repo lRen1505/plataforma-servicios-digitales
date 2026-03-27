f5f0b68205ec440d799309205c37a0bc6a256d7f
lRen1505 <renso.escuadra@usil.pe>
2026-03-27 10:27:20 -0500

    Conectamos el servicio a la db

--- controllers/servicioController.js
+++ controllers/servicioController.js
@@ -1,26 +1,18 @@
-const servicios = [
-  {
-    id: 1,
-    nombre: "Desarrollo de página web",
-    descripcion: "Creación de landing page o sitio web corporativo",
-    precio: 300 
-  },
-  {
-    id: 2,
-    nombre: "Diseño de logo",
-    descripcion: "Diseño de identidad visual para marca o negocio",
-    precio: 80
-  },
-  {
-    id: 3,
-    nombre: "Dashboard en Power BI",
-    descripcion: "Desarrollo de reportes y paneles interactivos",
-    precio: 150
-  }
-];
+const db = require("../config/db");
 
 const obtenerServicios = (req, res) => {
-  res.json(servicios);
+  const sql = "SELECT * FROM servicios";
+
+  db.query(sql, (error, results) => {
+    if (error) {
+      console.error("Error al obtener servicios:", error);
+      return res.status(500).json({
+        mensaje: "Error al obtener servicios"
+      });
+    }
+
+    res.json(results);
+  });
 };
 
 module.exports = {
